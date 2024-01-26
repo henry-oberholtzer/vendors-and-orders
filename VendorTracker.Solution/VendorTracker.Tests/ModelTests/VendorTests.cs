654105cf626bdc923d02a447e4db8bab6c3804be
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-26 09:25:18 -0800

    pass second test

--- VendorTracker.Solution/VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Solution/VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -8,7 +8,14 @@ public class VendorTests
   [TestMethod]
   public void Vendor_ShouldCreateAnInstanceOfVendor_Vendor()
   {
-    Vendor one = new();
+    Vendor one = new("one");
     Assert.AreEqual(one.GetType(), typeof(Vendor));
+  }
+  [TestMethod]
+  public void Vendor_GetName_String()
+  {
+    string expected = "two";
+    Vendor two = new(expected);
+    Assert.AreEqual(two.Name, expected);
   }
 }
---
+++ VendorTracker.Solution/VendorTracker/Models/Vendor.cs
@@ -0,0 +1,10 @@
+namespace VendorTracker.Models;
+
+public class Vendor
+{
+  public string Name { get; set; }
+  public Vendor (string name)
+  {
+    Name = name;
+  }
+}
