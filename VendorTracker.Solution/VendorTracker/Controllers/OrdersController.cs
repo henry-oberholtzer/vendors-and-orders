10c8a37c23a6b50fa499c1b170cf3109ca3cdae7
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-26 19:00:16 -0800

    now possible to add items to a vendor

--- VendorTracker.Solution/VendorTracker/Controllers/OrdersController.cs
+++ VendorTracker.Solution/VendorTracker/Controllers/OrdersController.cs
@@ -5,21 +5,48 @@ namespace VendorTracker.Controllers;
 
 public class OrdersController : Controller
 {
+  [HttpGet("/vendors/{vendorId}/orders")]
+    public ActionResult Index(string vendorId)
+  {
+    Vendor target = Vendor.Find(vendorId);
+    return View(target);
+  }
+
   [HttpGet("/vendors/{id}/orders/new")]
   public ActionResult New(string id)
   {
-    Dictionary<string, string> Model = new()
-    {
-        { "Id", id }
-    };
-    return View(Model);
+    Vendor target = Vendor.Find(id);
+    return View(target);
   }
+  [HttpPost("/vendors/{vendorId}/orders")]
+  public ActionResult Create(string vendorId, string title, string price, string description)
+  {
+    string titleClean = title.Trim();
+    if(int.TryParse(price, out int priceNum) && titleClean.Length > 0)
+    {
+          Order newOrder = new(title, priceNum, description);
+          Vendor.Find(vendorId).AddOrder(newOrder);
+          RouteValueDictionary route = new(){
+            {"id", vendorId},
+            { "orderId", newOrder.Id}
+            };
+          return RedirectToAction("Show", route);
+    }
+    else
+    {
+      RouteValueDictionary route = new(){{"id", vendorId}};
+      return RedirectToAction("New", route);
+    }
+  }
+
+
   [HttpGet("/vendors/{id}/orders/{orderId}")]
   public ActionResult Show(string orderId)
   {
     Order target = Order.Find(orderId);
     return View(target);
   }
-}
+
+};
 
 
